e1c8bf4843de33e5dd8e9259b522587c9272f1bb
PoorRijan <rijan_dhakal2055@protonmail.com>
2025-04-16 07:03:45 -0500

    Moving the nvim config here

---
+++ configs/nvim/init.lua
@@ -0,0 +1,15 @@
+require("plugins")
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+-- Keymappings
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+
+vim.cmd.colorscheme "catppuccin"
--- modules/home-manager/rijan.nix
+++ modules/home-manager/rijan.nix
@@ -172,6 +172,19 @@
       zotero
     ];
 
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    # Link the nvim config directory from the flake root
+    xdg.configFile."nvim" = {
+      # Path from rijan.nix up two levels (to root) then into nvim/
+      source = ../../configs/nvim;
+      recursive = true;
+    };
+
     programs.atuin = {
       enable = true;
       enableFishIntegration = true;
